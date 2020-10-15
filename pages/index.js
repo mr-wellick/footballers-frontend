be0b60a47a185bf70d38ba4a734bb5a83dce9854
mr-wellick <lorenz3192@gmail.com>
2020-10-14 20:35:20 -0700

    add styles

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,12 @@
 import React from "react";
-import { useEffect, useState } from "react";
 
 const Home = () => {
-  const [seasons, setSeasons] = useState([]);
-  useEffect(() => {
-    fetch("http://localhost:5000/api/v1/seasons")
-      .then(res => res.json())
-      .then(data => {
-        const seasonValues = data.map(item => item.Tables_in_poc_config);
-        setSeasons(seasonValues);
-      })
-      .catch(err => console.error(err));
-  }, []);
-
-  return seasons.map(season => <p key={season}>{season}</p>);
+  return (
+    <div>
+      <h1>Welcome home!</h1>
+      <p>Let's get started.</p>
+    </div>
+  );
 };
 
 export default Home;
