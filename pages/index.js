4b6eead5715fbaf76bc99fd65f341c7176d880e7
mr-wellick <lorenz3192@gmail.com>
2020-10-17 18:45:34 -0700

    Test out data with recharts and rewrite component

--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,64 @@
+import {
+  BarChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Bar,
+  Legend,
+  Tooltip
+} from "recharts";
+import filter from "lodash.filter";
+
 /** @jsx jsx */
 import { jsx } from "theme-ui";
-import { Box } from "theme-ui";
-import { Select } from "theme-ui";
-import { useState, useEffect } from "react";
+import { Container } from "theme-ui";
 
-const useHandler = e => {
-  return async e => {
-    const { value } = e.target;
-    const res = await fetch("http://localhost:5000/api/v1/season", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ season: value })
-    });
-
-    try {
-      const data = await res.json();
-      console.log(data);
-      return;
-    } catch (err) {
-      console.error(err);
-    }
-  };
-};
-
-const Home = () => {
-  const [seasons, setSeasons] = useState([]);
-  const handler = useHandler();
-
-  useEffect(() => {
-    fetch("http://localhost:5000/api/v1/seasons")
-      .then(res => res.json())
-      .then(data => {
-        const filteredData = data.map(datum => datum.Tables_in_poc_config);
-        setSeasons(filteredData);
-      })
-      .catch(err => console.error(err));
-  }, []);
+const Home = ({ data }) => {
+  const forwards = filter(data, data => data.pos === "F");
 
   return (
     <div sx={{ variant: "containers.page" }}>
-      <Box
-        sx={{ padding: "0 40px", paddingTop: "24px", paddingBottom: "20px" }}
-      >
-        <Select defaultValue="Select a season" sx={{ width: "50%" }}>
-          {seasons.map(season => (
-            <option key={season} value={season} onClick={handler}>
-              {season.replace("_", " ")}
-            </option>
-          ))}
-        </Select>
-      </Box>
+      <Container p={4}>
+        <BarChart
+          width={1500}
+          height={500}
+          data={forwards}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 20,
+            bottom: 10
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Bar dataKey="app" fill="#22b2c7" />
+          <Bar dataKey="g" fill="#dda4a7" />
+          <Bar dataKey="a" fill="#a2ad66" />
+          <Tooltip />
+          <Legend />
+        </BarChart>
+      </Container>
     </div>
   );
 };
+
+export async function getStaticProps() {
+  const res = await fetch("http://localhost:5000/api/v1/season", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ season: "season_2008" })
+  });
+  const data = await res.json();
+
+  return {
+    props: {
+      data
+    }
+  };
+}
 
 export default Home;
