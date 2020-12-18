52f6107b52478ffe49d5b3f5d8d3a69fa85f0e39
mr-wellick <lorenz3192@gmail.com>
2020-12-18 07:48:03 -0800

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   purge: ["./pages/**/*.js", "./src/components/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      consoleSidebar: "#0f1923", // dark blue
+      inactiveSidebarItem: "#a3b2c3", // pale blue
+      activeSidebarItem: "#202a35", // ligthest palest blue
+    },
     extend: {},
   },
   variants: {
