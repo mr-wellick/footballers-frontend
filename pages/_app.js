d3a0d2bff01f0019b8d58df9a3e12c5881fe56ec
mr-wellick <lorenz3192@gmail.com>
2020-12-18 07:36:05 -0800

    add tailwindcss

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { store } from "../src/redux/";
+import "tailwindcss/tailwind.css";
 
 const App = ({ Component, pageProps }) => {
   return (
