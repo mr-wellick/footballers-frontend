fdd1de540363953aa86d9c3b7fad78f776a7a418
mr-wellick <lorenz3192@gmail.com>
2021-01-10 15:27:58 -0800

    added console chart layout

--- src/components/footballers-chart.js
+++ src/components/footballers-chart.js
@@ -27,20 +27,10 @@ const FootballersChart = () => {
   }
 
   return (
-    <BarChart
-      width={1000}
-      height={500}
-      data={season}
-      margin={{
-        top: 5,
-        right: 30,
-        left: 20,
-        bottom: 5,
-      }}
-    >
+    <BarChart width={1100} height={500} data={season}>
       <CartesianGrid strokeDasharray="3 3" />
       <XAxis dataKey="name" />
-      <YAxis />
+      <YAxis orientation="right" />
       <Tooltip />
       <Legend />
       <Bar dataKey="g" fill="#8884d8" />
